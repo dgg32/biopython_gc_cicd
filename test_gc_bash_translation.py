f008c7aadbb5b277e2ba549c2d21e70a570720c0
huangsixing <dgg321982@gmail.com>
2022-08-15 14:16:03 +0800

    first commit

---
+++ test_gc_bash_translation.py
@@ -0,0 +1,8 @@
+import gc_bash_translation
+
+def test_calculate_gc_1():
+    assert gc_bash_translation.calculate_gc("ATGCGC") == 66
+
+def test_calculate_gc_2():
+    assert gc_bash_translation.calculate_gc("ATGCAT") == 33
+
