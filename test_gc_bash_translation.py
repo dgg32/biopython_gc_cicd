cc797386b77be825af9efb20c1a499fc407993d5
huangsixing <dgg321982@gmail.com>
2022-08-15 15:43:31 +0800

    github init

--- test_gc_bash_translation.py
+++ test_gc_bash_translation.py
@@ -1,7 +1,7 @@
 import gc_bash_translation
 
 def test_calculate_gc_1():
-    assert gc_bash_translation.calculate_gc("ATGCGC") == 66
+    assert gc_bash_translation.calculate_gc("ATCCGC") == 66
 
 def test_calculate_gc_2():
     assert gc_bash_translation.calculate_gc("ATGCAT") == 33
